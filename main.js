14c009e03195a4a907c0db2bcfb28ed86b28ba90
Imadeddine Belarbi <belarbi.imadeddine03@gmail.com>
2023-10-28 21:03:52 +0100

    update

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-let currentInput = '';
+let currentInput = '0';
+let currentOperation = '0';
 let lastChar = '';
-let currentNumber = '';
-const symbols = ['+', '-', '%', '*', '/', '.'];
+const symbols = ['+', '-', '*', '/', '.'];
 
-const display = document.querySelector('textarea');
+const display = document.querySelector('input');
+display.value = currentOperation;
 
 function isAsymbol(v)
 {
@@ -15,32 +16,74 @@ function isAsymbol(v)
 
 function addToDisplay(v)
 {
+  if (currentInput.length > 16)
+    return;
+
+  if (currentInput === '0')
+    currentInput = '';
   lastChar = currentInput.length > 0 ? currentInput.charAt(currentInput.length - 1) : '+';
   if (!isAsymbol(lastChar) || !isAsymbol(v))
   {
     currentInput += v;
     display.value = currentInput;
+    currentOperation += v;
+  }
+}
+
+function addPercentToDisplay()
+{
+  if (currentInput.length > 16)
+    return;
+  lastChar = currentInput.length > 0 ? currentInput.charAt(currentInput.length - 1) : '+';
+  if (!isAsymbol(lastChar))
+  {
+    currentInput += '%';
+    display.value = currentInput;
+    currentOperation += '/100';
   }
 }
 
 function clearDisplay()
 {
-  currentInput = '';
-  display.value = '';
+  currentInput = '0';
+  currentOperation = '0'
+  display.value = '0';
 }
 
 function backspaceDisplay()
 {
+  if (currentInput === '0')
+    return;
+
+  lastChar = currentInput.charAt(currentInput.length - 1);
+  currentOperation = lastChar === '%' ? currentOperation.slice(0, -1) : currentOperation.slice(0, -4);
   currentInput = currentInput.slice(0, -1);
+
+  if (currentInput === '')
+    currentInput = '0';
   display.value = currentInput;
 }
 
 function calculate()
 {
-  currentInput = eval(currentInput).toString();
-  if (currentInput === "Infinity")
-    display.value = "Divsion by zero"
-  else
-    display.value = currentInput;
-
+  try
+  {
+    currentOperation = eval(currentOperation).toString();
+    if (currentOperation === "Infinity")
+    {
+      currentInput = '0';
+      currentOperation = '0';
+      display.value = "Divsion by zero"
+    }
+    else
+    {
+      currentInput = currentOperation;
+      display.value = currentOperation;
+    }
+  }
+  catch {
+    currentInput = '0';
+    currentOperation = '0';
+    display.value = "Error";
+  }
 }
